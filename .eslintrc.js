b4b5c89adbb82f4a5df0f648ba790110f3c8ac8c
Angus Croll <angus@uber.com>
2019-01-11 11:45:24 -0800

    ready

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
-/* eslint-env node */
 module.exports = {
-  extends: [require.resolve('eslint-config-fusion')],
-  rules: {
-    "no-debugger": 0,
-    "flowtype/require-valid-file-annotation": 0
-  }
+  extends: [
+    require.resolve('eslint-config-fusion')
+  ]
 };
